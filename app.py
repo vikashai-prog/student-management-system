405c546273a2fa6afbdfae6ae0ba96384c17d63d
vikashai-prog <vikashkumary000@gmail.com>
2026-04-06 10:29:18 +0530

    final fix for render port

--- app.py
+++ app.py
@@ -111,4 +111,4 @@ def chat():
 import os
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
